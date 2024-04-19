88de87d156fe50296f9817203a3964d798bf3924
AndreasEkvall <anek0001@stud.kea.dk>
2024-04-19 11:02:31 +0200

    more stuff

---
+++ src/app/add-event/page.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { redirect } from 'next/navigation'
+
+export default async function AddEventPage() {
+  async function submit(formData) {
+    "use server"
+
+    let headersList = {
+      Accept: "application/json",
+      apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      Prefer: "return=representation",
+      "Content-Type": "application/json",
+     }
+     
+     let bodyContent = JSON.stringify(  {
+        name: formData.get("name"),
+        when: formData.get("when"),
+        description: formData.get("comment"),
+       });
+     
+     let response = await fetch("https://nyguwfyqotcwrephctpb.supabase.co/rest/v1/eventplanning", { 
+       method: "POST",
+       body: bodyContent,
+       headers: headersList
+     });
+
+     let data = await response.json();
+     const id = data[0].id;
+     console.log(id);
+     console.log(formData);
+     redirect("/event/" + id)
+
+  }
+  return (
+    <form className="grid grid-rows-3 w-1/2 "action={submit}>
+        <div className="formcontrol">
+            <label htmlFor="form_name">Title</label>
+            <input id="form_name" name="name" type="text" required/>
+        </div>
+        <div className="formcontrol">
+            <label htmlFor="form_when">When</label>
+            <input id="form_when" name="when" type="date" required/>
+        </div>
+        <div className="formcontrol">
+            <label htmlFor="form_comment">Any comments?</label>
+            <input id="form_comment" name="comment" type="text" required/>
+        </div>
+        <button>Get</button>
+    </form>
+  )
+}
---
+++ src/app/event/[uuid]/page.jsx
@@ -0,0 +1,31 @@
+export default async function EventPage({ params }) {
+    console.log(params);
+    const uuid = params.uuid;
+
+    let headersList = {
+        Accept: "application/json",
+        apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+       }
+       
+    let response = await fetch("https://nyguwfyqotcwrephctpb.supabase.co/rest/v1/eventplanning/id.eq." + uuid, { 
+      headers: headersList
+    });
+
+    let data = await response.json();
+    const eventInfo = data[0];
+    console.log("Data: ", data)
+    console.log("Eventinfo: ",eventInfo)
+    
+    return (
+      <article>
+        <h1>Hej</h1>
+        {/* <h1>{eventInfo.name}</h1>
+        <dl>
+          <dt>When</dt>
+          <dd>{eventInfo.when}</dd>
+          <dd>{eventInfo.description}</dd>
+        </dl> */}
+      </article>
+
+  );
+}
