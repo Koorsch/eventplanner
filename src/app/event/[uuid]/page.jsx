c0de1cd148ec121f02174179d5f6debcea6870a3
AndreasEkvall <anek0001@stud.kea.dk>
2024-04-19 11:10:31 +0200

    fixes to uuid

--- src/app/event/[uuid]/page.jsx
+++ src/app/event/[uuid]/page.jsx
@@ -7,24 +7,24 @@ export default async function EventPage({ params }) {
         apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
        }
        
-    let response = await fetch("https://nyguwfyqotcwrephctpb.supabase.co/rest/v1/eventplanning/id.eq." + uuid, { 
+    let response = await fetch("https://nyguwfyqotcwrephctpb.supabase.co/rest/v1/eventplanning?id.eq." + uuid, { 
       headers: headersList
     });
 
     let data = await response.json();
     const eventInfo = data[0];
     console.log("Data: ", data)
-    console.log("Eventinfo: ",eventInfo)
+    console.log("Eventinfo: ", eventInfo)
     
     return (
       <article>
         <h1>Hej</h1>
-        {/* <h1>{eventInfo.name}</h1>
+        <h1>{eventInfo.name}</h1>
         <dl>
           <dt>When</dt>
           <dd>{eventInfo.when}</dd>
           <dd>{eventInfo.description}</dd>
-        </dl> */}
+        </dl>
       </article>
 
   );
